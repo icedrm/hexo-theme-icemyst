e9beb4cd3693444cdc7376c4b367ac1d4eed5743
冰梦 <1580777927@qq.com>
2024-08-13 19:38:08 +0800

    tags 整改

--- themes/butterfly/scripts/helpers/page.js
+++ themes/butterfly/scripts/helpers/page.js
@@ -1,88 +1,81 @@
-'use strict'
+'use strict';
 
-const { stripHTML, escapeHTML, prettyUrls } = require('hexo-util')
-const crypto = require('crypto')
+const { stripHTML, escapeHTML, prettyUrls } = require('hexo-util');
+const crypto = require('crypto');
 
+// 注册页面描述生成器
 hexo.extend.helper.register('page_description', function () {
-  const { config, page } = this
-  let description = page.description || page.content || page.title || config.description
+  const { config, page } = this;
+  const description = escapeHTML(stripHTML(page.description || page.content || page.title || config.description || '')
+    .substring(0, 150).trim()).replace(/\n/g, ' ');
 
-  if (description) {
-    description = escapeHTML(stripHTML(description).substring(0, 150)
-      .trim()
-    ).replace(/\n/g, ' ')
-    return description
-  }
-})
+  return description;
+});
 
-hexo.extend.helper.register('cloudTags', function (options = {}) {
-  const env = this
-  let { source, minfontsize, maxfontsize, limit, unit, orderby, order } = options
-  unit = unit || 'px'
+// 注册标签云生成器
+hexo.extend.helper.register('cloudTags', function ({
+  source,
+  fontsize = 1, // 默认字体大小
+  limit = 0,
+  unit = 'px',
+  orderby = 'name',
+  order = 1
+} = {}) {
+  const env = this;
+  let result = '';
 
-  let result = ''
   if (limit > 0) {
-    source = source.limit(limit)
+    source = source.limit(limit);
   }
 
-  const sizes = []
-  source.sort('length').forEach(tag => {
-    const { length } = tag
-    if (sizes.includes(length)) return
-    sizes.push(length)
-  })
-
-  const length = sizes.length - 1
   source.sort(orderby, order).forEach(tag => {
-    const ratio = length ? sizes.indexOf(tag.length) / length : 0
-    const size = minfontsize + ((maxfontsize - minfontsize) * ratio)
-    let style = `font-size: ${parseFloat(size.toFixed(2))}${unit};`
-    const color = 'rgb(' + Math.floor(Math.random() * 201) + ', ' + Math.floor(Math.random() * 201) + ', ' + Math.floor(Math.random() * 201) + ')' // 0,0,0 -> 200,200,200
-    style += ` color: ${color}`
-    result += `<a href="${env.url_for(tag.path)}" style="${style}">${tag.name}</a>`
-  })
-  return result
-})
+    const style = `font-size: ${fontsize}${unit}; color: rgb(${Math.floor(Math.random() * 201)}, ${Math.floor(Math.random() * 201)}, ${Math.floor(Math.random() * 201)});`;
+    result += `<a href="${env.url_for(tag.path)}" style="${style}"><span class='icon'># </span>${tag.name}<sup class='tag_num' style="font-style:italic;">${tag.length}</sup></a>`;
+  });
 
+  return result;
+});
+
+// 注册无索引的URL生成器
 hexo.extend.helper.register('urlNoIndex', function (url = null, trailingIndex = false, trailingHtml = false) {
-  return prettyUrls(url || this.url, { trailing_index: trailingIndex, trailing_html: trailingHtml })
-})
+  return prettyUrls(url || this.url, { trailing_index: trailingIndex, trailing_html: trailingHtml });
+});
 
+// 注册MD5生成器
 hexo.extend.helper.register('md5', function (path) {
-  return crypto.createHash('md5').update(decodeURI(this.url_for(path))).digest('hex')
-})
+  return crypto.createHash('md5').update(decodeURI(this.url_for(path))).digest('hex');
+});
 
+// 注册HTML注入器
 hexo.extend.helper.register('injectHtml', function (data) {
-  if (!data) return ''
-  return data.join('')
-})
+  return data ? data.join('') : '';
+});
 
+// 注册归档标题查找器
 hexo.extend.helper.register('findArchivesTitle', function (page, menu, date) {
   if (page.year) {
-    const dateStr = page.month ? `${page.year}-${page.month}` : `${page.year}`
-    const dateFormat = page.month ? hexo.theme.config.aside.card_archives.format : 'YYYY'
-    return date(dateStr, dateFormat)
+    const dateStr = `${page.year}${page.month ? `-${page.month}` : ''}`;
+    const dateFormat = page.month ? hexo.theme.config.aside.card_archives.format : 'YYYY';
+    return date(dateStr, dateFormat);
   }
 
-  const defaultTitle = this._p('page.archives')
-  if (!menu) return defaultTitle
-
+  const defaultTitle = this._p('page.archives');
   const loop = (m) => {
     for (const key in m) {
       if (typeof m[key] === 'object') {
-        loop(m[key])
-      }
-
-      if (/\/archives\//.test(m[key])) {
-        return key
+        const foundTitle = loop(m[key]);
+        if (foundTitle) return foundTitle;
+      } else if (/\/archives\//.test(m[key])) {
+        return key;
       }
     }
-  }
+  };
 
-  return loop(menu) || defaultTitle
-})
+  return loop(menu) || defaultTitle;
+});
 
+// 注册图片或URL检测器
 hexo.extend.helper.register('isImgOrUrl', function (path) {
-  const imgTestReg = /\.(png|jpe?g|gif|svg|webp)(\?.*)?$/i
-  return path.indexOf('//') !== -1 || imgTestReg.test(path)
-})
+  const imgTestReg = /\.(png|jpe?g|gif|svg|webp)(\?.*)?$/i;
+  return path.includes('//') || imgTestReg.test(path);
+});
